592b00c882c94ef2ce4fda52c02e9521ecca90af
lcy0330 <281740119@qq.com>
2026-09-19 17:21:33 +0800

    Refactor to use constructServer for app initialization

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-require('./app.js')
+const { constructServer } = require('./server')
+
+let appPromise = null
+
+module.exports = async (req, res) => {
+  if (!appPromise) {
+    appPromise = constructServer()
+  }
+  const app = await appPromise
+  return app(req, res)
+}
